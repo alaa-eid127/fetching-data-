305c055ce00ce4573b8196aafddace0234ac17ad
alaa-eid127 <engalaa127@gmail.com>
2022-07-05 01:13:12 +0200

    fetching data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const getData = () => {
+    fetch('https://jsonplaceholder.typicode.com/posts?_limit=9')
+      .then(response => response.json())
+      .then((result) => {
+        setData(result)
+        setError(null)
+      }).catch((err) => {
+        setError(err.message);
+        setData([]);
+      }).finally(() => {
+        setLoading(false);
+      })
+  }
+  console.log(data)
+  useEffect(() => {
+    getData()
+  }, [])
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='title'>API Post</h1>
+      {loading && <div> A moment please...</div>}
+      {error && <div>`there is a problem fetching the post data-{error}`</div>}
+      {data.map(item => (
+        <div className='myPosts' key={item.id}>
+          <h1 className='read'>{item.title}</h1>
+        </div>
+      ))}
     </div>
   );
 }
